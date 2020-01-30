173c788c0f608911c78e36cec6de3da6b75438e0
Ilya Irbitskiy <castlingcheckmate97@gmail.com>
2020-01-30 19:26:44 +0300

    Hyperedges drawing

--- Diploma.UI/Views/Controls/HypergraphView.xaml.cs
+++ Diploma.UI/Views/Controls/HypergraphView.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
@@ -13,21 +14,26 @@ namespace Diploma.UI.Views.Controls
     public partial class HypergraphView : UserControl
     {
 
-        private readonly int VertexDiameter = 20;
+        private readonly int VertexDiameter = 40;
 
         private Dictionary<Ellipse, VertexSimplicesView> _vertexSimplicesViews;
+        private Dictionary<Ellipse, Line[]> _simplicesViews;
 
         public HypergraphView()
         {
             InitializeComponent();
             _vertexSimplicesViews = new Dictionary<Ellipse, VertexSimplicesView>();
+            _simplicesViews = new Dictionary<Ellipse, Line[]>();
             Loaded += (sender, eventArgs) =>
             {
                 Window.GetWindow(this).Closing += (_sender, _eventArgs) =>
                 {
                     foreach (var vertex in _canvas.Children.OfType<Ellipse>().ToArray())
                     {
-                        (vertex.Tag as Timer).Dispose();
+                        if (!(vertex.Tag is null) && !(vertex.Tag is bool))
+                        {
+                            (vertex.Tag as Timer).Dispose();
+                        }
                         if (_vertexSimplicesViews.ContainsKey(vertex))
                         {
                             (_vertexSimplicesViews[vertex].Tag as Timer).Dispose();
@@ -50,19 +56,26 @@ namespace Diploma.UI.Views.Controls
 
         public void Redraw()
         {
-            foreach (var vertex in _canvas.Children.OfType<Ellipse>().ToArray())
+            foreach (var vertexOrSimplexCenter in _canvas.Children.OfType<Ellipse>().ToArray())
             {
-                (vertex.Tag as Timer).Dispose();
-                _canvas.Children.Remove(vertex);
-                if (_vertexSimplicesViews.Keys.Contains(vertex))
+                if (!(vertexOrSimplexCenter.Tag is null) && !(vertexOrSimplexCenter.Tag is bool))
                 {
-                    (_vertexSimplicesViews[vertex].Tag as Timer).Dispose();
-                    _vertexSimplicesViews.Remove(vertex);
+                    (vertexOrSimplexCenter.Tag as Timer).Dispose();
+                }
+                _canvas.Children.Remove(vertexOrSimplexCenter);
+                if (_vertexSimplicesViews.Keys.Contains(vertexOrSimplexCenter))
+                {
+                    (_vertexSimplicesViews[vertexOrSimplexCenter].Tag as Timer).Dispose();
+                    _vertexSimplicesViews.Remove(vertexOrSimplexCenter);
+                }
+                else if (_simplicesViews.Keys.Contains(vertexOrSimplexCenter))
+                {
+                    _simplicesViews.Remove(vertexOrSimplexCenter);
                 }
             }
-            foreach (var simplex in _canvas.Children.OfType<Polyline>().ToArray())
+            foreach (var simplexPart in _canvas.Children.OfType<Line>().ToArray())
             {
-                _canvas.Children.Remove(simplex);
+                _canvas.Children.Remove(simplexPart);
             }
             if (Hypergraph is null)
             {
@@ -72,11 +85,73 @@ namespace Diploma.UI.Views.Controls
             // simplices
             foreach (var simplex in Hypergraph.Simplices)
             {
-                var simplexView = new Polyline();
-                simplexView.Stroke = Brushes.Black;
-                for (var i = 0; i < simplex.Vertices.Length; i++)
+                var verticesCenterPoints = simplex.Vertices.Select(vertex => GetVertexCenterPoint(vertex.Id));
+                var simplexCenterPoint = new Point(verticesCenterPoints.Select(vertex => vertex.X).Average(), verticesCenterPoints.Select(vertex => vertex.Y).Average());
+                var simplexCenter = new Ellipse()
                 {
-                    simplexView.Points.Add(GetVertexCenterPoint(simplex.Vertices[i].Id));
+                    Width = VertexDiameter / 2
+                    , Height = VertexDiameter / 2
+                    , Fill = Brushes.Gray
+                    , Margin = new Thickness(simplexCenterPoint.X - VertexDiameter / 4, simplexCenterPoint.Y - VertexDiameter / 4, 0, 0)
+                    , Tag = false
+                };
+                simplexCenter.MouseEnter += (sender, eventArgs) =>
+                {
+                    simplexCenter.Fill = Brushes.Black;
+                    for (var i = 0; i < _simplicesViews[simplexCenter].Length; i++)
+                    {
+                        _simplicesViews[simplexCenter][i].Stroke = Brushes.Black;
+                    }
+                };
+                simplexCenter.MouseLeave += (sender, eventArgs) =>
+                {
+                    simplexCenter.Fill = Brushes.Gray;
+                    for (var i = 0; i < _simplicesViews[simplexCenter].Length; i++)
+                    {
+                        _simplicesViews[simplexCenter][i].Stroke = Brushes.Gray;
+                    }
+                };
+                simplexCenter.MouseDown += (sender, eventArgs) =>
+                {
+                    simplexCenter.Tag = true;
+                };
+                simplexCenter.MouseUp += (sender, eventArgs) =>
+                {
+                    simplexCenter.Tag = false;
+                };
+                simplexCenter.MouseMove += (sender, eventArgs) =>
+                {
+                    if (!(bool)simplexCenter.Tag)
+                    {
+                        return;
+                    }
+                    simplexCenter.Margin = new Thickness(eventArgs.GetPosition(_canvas).X - VertexDiameter / 4, eventArgs.GetPosition(_canvas).Y - VertexDiameter / 4, 0, 0);
+                    for (var i = 0; i < _simplicesViews[simplexCenter].Length; i++)
+                    {
+                        _simplicesViews[simplexCenter][i].X1 = eventArgs.GetPosition(_canvas).X;
+                        _simplicesViews[simplexCenter][i].Y1 = eventArgs.GetPosition(_canvas).Y;
+                    }
+                };
+                var simplexEdges = new Line[simplex.Vertices.Length];
+                for (var i = 0; i < simplexEdges.Length; i++)
+                {
+                    simplexEdges[i] = new Line()
+                    {
+                        X1 = simplexCenterPoint.X
+                        , Y1 = simplexCenterPoint.Y
+                        , X2 = GetVertexCenterPoint(simplex.Vertices[i].Id).X
+                        , Y2 = GetVertexCenterPoint(simplex.Vertices[i].Id).Y
+                        , Stroke = Brushes.Gray
+                        , StrokeThickness = 1
+                    };
+                }
+                _simplicesViews.Add(simplexCenter, simplexEdges);
+            }
+            foreach (var simplexView in _simplicesViews.Keys)
+            {
+                foreach (var simplexPart in _simplicesViews[simplexView])
+                {
+                    _canvas.Children.Add(simplexPart);
                 }
                 _canvas.Children.Add(simplexView);
             }
